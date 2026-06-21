d5526f7fb00ac5da44c81eac0ba6151bf2718e99
Ankitsawant19 <ankitsawant1904@gmail.com>
2026-06-21 17:36:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -55,6 +55,8 @@ User symptoms: ${message}
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
